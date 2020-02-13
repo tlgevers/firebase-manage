2133dfeb7ef5fb8504093231ae3021532f283e18
Trevor Gevers <trevor.gevers@ditoweb.com>
2020-02-13 09:05:57 -0600

    allow user to enter project ID

--- generate-firebase-config.sh
+++ generate-firebase-config.sh
@@ -1,5 +1,16 @@
 #!/usr/bin/env bash
 
-export PROJECT_ID="didi-dito-trevor"
+echo Please enter a project ID:
+read PROJECT_ID
+
+if [ -z "$PROJECT_ID" ]
+then
+    echo "Please enter a project ID"
+    exit 1
+else
+    echo "Using " $PROJECT_ID
+fi
+
+export PROJECT_ID=$PROJECT_ID
 
 python main.py --action=getConfig
